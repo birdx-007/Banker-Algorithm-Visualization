cb0412cf9a09c7d2e8717e77dccf0f84b041c83d
BirdNoX007 <birdnox007@163.com>
2023-06-02 16:57:38 +0800

    20230602 milestone: fix bugs and test.

--- bankmanager.cpp
+++ bankmanager.cpp
@@ -1,4 +1,5 @@
 #include "bankmanager.h"
+#include <QDebug>
 
 BankManager::BankManager()
 {
@@ -15,6 +16,7 @@ bool BankManager::safetyCheck()
     const int clientNumber = dataAccess->allocation.size();
     std::vector<bool> finish(clientNumber,false);
     std::array<int,Constant::ResourceTypeNum> work(dataAccess->available);
+    std::vector<int> safeAllocationSequence;
     bool existAllocatableClient;
     int allocatableClientIndex;
     do
@@ -35,6 +37,7 @@ bool BankManager::safetyCheck()
             if(existAllocatableClient)
             {
                 allocatableClientIndex = i;
+                safeAllocationSequence.push_back(i);
                 break;
             }
         }
@@ -55,6 +58,12 @@ bool BankManager::safetyCheck()
             return false;
         }
     }
+    QString s = QString("Bank is safe, client sequence:");
+    for(int i=0;i<(int)safeAllocationSequence.size();i++)
+    {
+        s.append(QString(" %1").arg(safeAllocationSequence[i]));
+    }
+    qDebug()<<s.toStdString().c_str();
     return true;
 }
 
@@ -64,24 +73,37 @@ bool BankManager::bankerAlgorithm(const int &i, const RequestData &request)
     {
         if(request.resourceNeed[j]>dataAccess->need[i][j])
         {
+            qDebug()<<"Request is larger than client's need!";
             return false;
         }
+    }
+    for(int j=0;j<Constant::ResourceTypeNum;j++)
+    {
         if(request.resourceNeed[j]>dataAccess->available[j])
         {
+            qDebug()<<"Bank doesn't have enough resources for the request yet.";
             return false;
         }
+    }
+    for(int j=0;j<Constant::ResourceTypeNum;j++)
+    {
         // try allocation
         dataAccess->available[j]=dataAccess->available[j]-request.resourceNeed[j];
         dataAccess->allocation[i][j]=dataAccess->allocation[i][j]+request.resourceNeed[j];
         dataAccess->need[i][j]=dataAccess->need[i][j]-request.resourceNeed[j];
-        if(!safetyCheck())
+    }
+    bool isSafe = safetyCheck();
+    if(!isSafe)
+    {
+        for(int j=0;j<Constant::ResourceTypeNum;j++)
         {
             // undo allocation
             dataAccess->available[j]=dataAccess->available[j]+request.resourceNeed[j];
             dataAccess->allocation[i][j]=dataAccess->allocation[i][j]-request.resourceNeed[j];
             dataAccess->need[i][j]=dataAccess->need[i][j]+request.resourceNeed[j];
-            return false;
         }
+        qDebug()<<"Bank will be unsafe if execute the allocation.";
+        return false;
     }
     return true;
 }
